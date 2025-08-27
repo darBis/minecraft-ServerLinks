6845b1f57be548dbd7eb1533c5ae25f835d5cbf3
Martin Darebný <martin.darebny@gmail.com>
2025-08-27 22:16:35 +0200

    fix duplication of server links on player join; add support for builtin link types (web, support, ...)

--- src/main/java/dev/tarna/serverlinks/ServerLinks.java
+++ src/main/java/dev/tarna/serverlinks/ServerLinks.java
@@ -1,13 +1,18 @@
 package dev.tarna.serverlinks;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ServerLinks.ServerLink;
+import org.bukkit.ServerLinks.Type;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ServerLinks extends JavaPlugin {
-    public HashMap<String, URI> links = new HashMap<>();
+    private List<ServerLink> links = new ArrayList<>();
 
     @Override
     public void onEnable() {
@@ -15,7 +20,6 @@ public final class ServerLinks extends JavaPlugin {
 
         saveDefaultConfig();
         new ServerLinksReload(this);
-        new PlayerJoinListener(this);
         loadLinks();
 
         getLogger().info("ServerLinks has been enabled in " + (System.currentTimeMillis() - now) + "ms");
@@ -24,21 +28,50 @@ public final class ServerLinks extends JavaPlugin {
     @Override
     public void onDisable() {
         long now = System.currentTimeMillis();
-
+        var serverLinks = Bukkit.getServerLinks();
+        for (var link : links) {
+            serverLinks.removeLink(link);
+        }
         getLogger().info("ServerLinks has been disabled in " + (System.currentTimeMillis() - now) + "ms");
     }
 
+    Type getKnowType(String key) {
+        try {
+            return Type.valueOf(key);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
     void loadLinks() {
+
+        var serverLinks = Bukkit.getServerLinks();
+
+        // remove old links first from server state
+        for (var link : links) {
+            serverLinks.removeLink(link);
+        }
         links.clear();
+
         var keys = getConfig().getKeys(false);
         for (String key : keys) {
             String value = getConfig().getString(key);
             try {
-                links.put(key, new URI(value.replace("&", "§")));
+                ServerLink link;
+                Type type = getKnowType(key);
+                if (type != null) {
+                    getLogger().info("Adding server link of type " + type + " with url: " + value);
+                    link = serverLinks.addLink(type, new URI(value.replace("&", "§")));
+                } else {
+                    getLogger().info("Adding server link of '" + key + "' with url: " + value);
+                    link = serverLinks.addLink(Component.empty().content(key), new URI(value.replace("&", "§")));
+                }
+                links.add(link);
             } catch (URISyntaxException e) {
                 getLogger().warning("Invalid URI for key " + key + ": " + value);
             }
         }
+
         getLogger().info("Loaded " + keys.size() + " links");
     }
 }
